240c8f5d7c773cceefb79c7430ad8f530025f67f
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-21 20:03:07 +0600

    email password login registration added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDd1PgmWwsX3spbxqOJnZDc9sP-0CdEVg4",
+  authDomain: "react-firebase-authentic-57692.firebaseapp.com",
+  projectId: "react-firebase-authentic-57692",
+  storageBucket: "react-firebase-authentic-57692.appspot.com",
+  messagingSenderId: "135921321689",
+  appId: "1:135921321689:web:f69309ddf1fc4b4479e0c8"
+};
+
+export default firebaseConfig;
